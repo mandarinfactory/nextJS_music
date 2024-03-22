f8ea6a4fe0cffc407dfd26276095533e4210a1bb
Ho Jun Lee <hoojunee@gmail.com>
2024-03-22 21:58:33 +0900

    24.03.22
    migration

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-      domains: ["i.scdn.co"]
+    domains: ["i.scdn.co"],
   },
+  pageExtensions: ["mdx", "md", "jsx", "js", "tsx", "ts"],
 };
 
 export default nextConfig;
