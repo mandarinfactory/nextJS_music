91e6da5b6fd0e86e7e3b7784e3ef5c412bc44f01
Ho Jun Lee <hoojunee@gmail.com>
2024-03-31 16:10:55 +0900

    24.03.31
    첫 화면 로딩 수정중...

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,12 +1,36 @@
 "use client";
 
+import React, { useEffect } from "react";
 import { GetServerSideProps } from "next";
+import { SetterOrUpdater, useSetRecoilState } from "recoil";
 
 import Hero from "./components/Hero";
 import Sidebar from "./components/Sidebar";
+import { accessTokenState } from "../recoil/atom";
+import { getAccessTokenData } from "./api/token";
 import { REDIRECT_URL, SCOPE } from "../utils/constants";
 
 const Home = ({ spotifyAuthUrl }: { spotifyAuthUrl: string }) => {
+  const setAccessToken:SetterOrUpdater<string> = useSetRecoilState(accessTokenState);
+
+  useEffect(() => {
+    const getSaveAccessToken = localStorage.getItem("accessToken");
+    const filteredAccessToken = async () => {
+      try {
+        const data = await getAccessTokenData();
+        localStorage.setItem("accessToken", data.data.access_token);
+      } catch (error) {
+        console.error("데이터 에러:", error);
+      }
+    };
+
+    filteredAccessToken();
+
+    if (getSaveAccessToken) {
+      setAccessToken(getSaveAccessToken);
+    }
+  }, [setAccessToken]);
+  
   return (
     <Sidebar spotifyAuthUrl={spotifyAuthUrl}>
       <Hero />
